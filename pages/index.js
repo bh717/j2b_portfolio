e1d08fdb59dd02e46788395ee9ef693ae7be6c51
Yuri Corredor <YuriCorredor159@gmail.com>
2022-03-29 17:46:08 -0300

    Under development

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function index() {
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <img src="under-development.png" />
+        </div>
+    )
+}
