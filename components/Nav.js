23b8746c328b31bd2058d55d3b9cf391ead1af99
Yuri Corredor <YuriCorredor159@gmail.com>
2022-03-30 11:04:59 -0300

    Added base components

---
+++ components/Nav.js
@@ -0,0 +1,5 @@
+export default function Nav() {
+    return (
+        <div>Nav</div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 export default function index() {
     return (
-        <h1></h1>
+        <></>
     )
 }
