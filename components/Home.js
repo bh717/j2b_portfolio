92002c95fb67a64e9b6abfa1d343612a9791cb14
Yuri Corredor <YuriCorredor159@gmail.com>
2022-04-13 16:03:42 -0300

    Minor changes

--- components/Home.js
+++ components/Home.js
@@ -1,29 +1,76 @@
 import { useEffect, useState } from 'react'
+import { motion, useAnimation } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import ScrollIcon from './home/ScrollIcon'
 
-export default function Home() {
+const divVariants = {
+    hidden: {
+        y: 200,
+        opacity: 0
+    },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            delay: 0.3,
+            duration: 1
+        }
+    }
+}
 
+const innerDivVariants = {
+    hidden: {
+        y: 100,
+        opacity: 0
+    },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            delay: 1,
+            duration: 1.5
+        }
+    }
+}
+
+export default function Home() {
+    const controls = useAnimation()
     const { ref, inView } = useInView()
-    const [alreadySeen, setAlreadySeen] = useState()
+    const [alreadySeen, setAlreadySeen] = useState(false)
 
     useEffect(() => {
-        
-    }, [inView])
+        if (inView) {
+            console.log(`oi`)
+            controls.start('visible')
+            setAlreadySeen(true)
+        }
+        if (!inView && !alreadySeen) {
+            console.log(`not`)
+            controls.start('hidden')
+        }
+    }, [inView, controls])
 
     return (
         <>
         <ScrollIcon />
-        <section className='flex text-center justify-center items-center w-full my-36 font-home max-w-7xl mx-auto'>
-            <div 
+        <section className='flex text-center justify-center items-center w-full mt-28 font-home max-w-7xl mx-auto'>
+            <motion.div
+                variants={divVariants}
+                initial="hidden"
+                animate={controls}
                 ref={ref} 
-                className='flex justify-center mx-auto border-x-2 bg-home w-full h-[580px] bg-cover bg-center bg-no-repeat text-white'
+                className='flex justify-center mx-auto border-x-0 xl:border-x-2 bg-home w-full h-[580px] bg-cover bg-center bg-no-repeat text-white'
             >
-                <div className='z-[9999] justify-center flex flex-col p-5 h-fit bg-black bg-opacity-75 rounded-md'>
-                    <h1 className='text-6xl'>Yuri Corredor</h1>
-                    <h3 className='text-2xl'>Developer</h3>
-                </div>
-            </div>
+                <motion.div
+                    variants={innerDivVariants}
+                    initial="hidden"
+                    animate={controls}
+                    className='z-[9999] justify-center flex flex-col p-3 m-2 h-fit bg-black bg-opacity-75 rounded-md'
+                >
+                    <h1 className='text-4xl sm:text-6xl'>Yuri Corredor</h1>
+                    <h3 className='text-xl sm:text-2xl'>Developer</h3>
+                </motion.div>
+            </motion.div>
         </section>
         </>
     )
