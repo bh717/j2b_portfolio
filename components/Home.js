0fed399cebc9bc636e8f3a869819a734abebcd29
Yuri Corredor <YuriCorredor159@gmail.com>
2022-04-13 13:34:04 -0300

    Home page

--- components/Home.js
+++ components/Home.js
@@ -14,12 +14,12 @@ export default function Home() {
     return (
         <>
         <ScrollIcon />
-        <section className='flex w-full my-36 font-home'>
+        <section className='flex text-center justify-center items-center w-full my-36 font-home max-w-7xl mx-auto'>
             <div 
                 ref={ref} 
-                className='flex justify-center bg-home bg-opacity-60 from-sky-500 to-indigo-500 w-full h-[640px] bg-cover text-white'
+                className='flex justify-center mx-auto border-x-2 bg-home w-full h-[580px] bg-cover bg-center bg-no-repeat text-white'
             >
-                <div className='z-[9999] justify-center items-center flex flex-col p-5 h-fit bg-black bg-opacity-75 rounded-md'>
+                <div className='z-[9999] justify-center flex flex-col p-5 h-fit bg-black bg-opacity-75 rounded-md'>
                     <h1 className='text-6xl'>Yuri Corredor</h1>
                     <h3 className='text-2xl'>Developer</h3>
                 </div>
